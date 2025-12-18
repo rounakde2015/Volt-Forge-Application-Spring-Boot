06353a280e425c6cdf7f5f434a58978ffd7b2492
Rounak De <rounakde2015@gmail.com>
2025-12-18 17:08:35 +0530

    initial setup
    initial setup of spring boot project

---
+++ src/main/java/com/voltforge/app/VoltForgeApplication.java
@@ -0,0 +1,13 @@
+package com.voltforge.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltForgeApplication.class, args);
+	}
+
+}
